02993aa3348d83c17c100477e3430bf088cbf75e
Matthew R. Laue <matt@zuar.com>
2019-07-18 09:27:02 -0700

    0.6.9.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if platform.system() == 'Darwin':
 
 setup(
     name="python-rocksdb",
-    version='0.6.9',
+    version='0.6.9.1',
     description="Python bindings for RocksDB",
     keywords='rocksdb',
     author='Ming Hsuan Tu',
